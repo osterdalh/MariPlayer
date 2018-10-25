d929bbd17a5ec178b2f1df7282bb9659ffadb2d1
Hans Østerdal <osterdalh@gmail.com>
2018-10-25 17:51:41 +0200

    script for treningsdata

--- fceux-2.2.3-win32/hello.lua
+++ fceux-2.2.3-win32/hello.lua
@@ -1,7 +1,23 @@
-while (true) do
-    input_player = joypad.read(1);
-    print(input_player);
+file = io.open("../training_data/inputs.txt", "w")
+file:write("A, left, right, up, down, B, start, select", "\n")
+frame = 0
+outDir = ".\\../training_data/images\\"
 
-    emu.frameadvance();
+    
+
+while (true) do
+    if frame % 4 == 0 then
+        input_player = joypad.read(1)
+        file:write(tostring((input_player["A"])) .. "," .. tostring((input_player["left"])) .. "," .. tostring((input_player["right"])) .. ","  .. tostring((input_player["up"])) .. ","  .. tostring((input_player["down"])) .. ","  .. tostring((input_player["B"])) .. ","  .. tostring((input_player["start"])) .. ","  .. tostring((input_player["select"])),"\n")
+        print(input_player)
+        --gui.savescreenshotas("frame_nr" .. tostring(frame) .. ".png", "../training_data/", 0)
+        gui.savescreenshotas(string.format("%s\\frame%d.png", outDir, frame))
+        print(input_player)
+    end
+    frame = frame + 1;
+    emu.frameadvance()
 
 end;
+file:close()
+
+
